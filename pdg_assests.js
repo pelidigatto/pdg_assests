50889f2857f65bb2a246bc56f1aeb55041908179
pelidigatto <florianthoenelt@outlook.de>
2020-10-07 12:37:56 +0200

    v0.01

--- pdg_assests.js
+++ pdg_assests.js
@@ -1,47 +1,40 @@
-	window.onload = init;
-        function init() {
-            if (localStorage.getItem("pdg_theme") === null) {
-                localStorage.setItem('pdg_theme', 'light');
-                
-            }
+window.onload = init;
+function init() {
+	if (localStorage.getItem("pdg_theme") === null) {
+		localStorage.setItem('pdg_theme', 'light');
 
-            
-
-            setTheme();
-        }
-        function setTheme(){
-            var theme = localStorage.getItem('pdg_theme');
-            var css_link = $("<link>", {
-                rel: "stylesheet",
-                type: "text/css",
-                href: "https://assets.peli-di-gatto.de/"+theme+".css"
-            });
-            css_link.appendTo('head');
-        }
-        function swicthDarkLightModeBTN(){
-            //localStorage.removeItem('darkTheme');
-            var new_theme = '';
-            var theme = localStorage.getItem('pdg_theme');
-            if(theme == 'dark'){
-                new_theme = 'light';
-            }else{
-                new_theme = 'dark';
-            }
-            localStorage.setItem('pdg_theme', new_theme);
-            setTheme();
-
-        }
-        /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    $('#mySidenav').css('width','250px');
-    $('body').css('marginLeft','250px');
-    $('.overlay').css('display','block');
+	}
+	setTheme();
 }
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function setTheme() {
+	var theme = localStorage.getItem('pdg_theme');
+	var css_link = $("<link>", {
+		rel: "stylesheet",
+		type: "text/css",
+		href: "https://assets.peli-di-gatto.de/" + theme + ".css"
+	});
+	css_link.appendTo('head');
+}
+function swicthDarkLightModeBTN() {
+	//localStorage.removeItem('darkTheme');
+	var new_theme = '';
+	var theme = localStorage.getItem('pdg_theme');
+	if (theme == 'dark') {
+		new_theme = 'light';
+	} else {
+		new_theme = 'dark';
+	}
+	localStorage.setItem('pdg_theme', new_theme);
+	setTheme();
+}
+function openNav() {
+	$('#mySidenav').css('width', '250px');
+	$('body').css('marginLeft', '250px');
+	$('.overlay').css('display', 'block');
+}
 function closeNav() {
-  $('#mySidenav').css('width','0');
-  $('body').css('marginLeft','0');
-  $('.overlay').css('display','none');
+	$('#mySidenav').css('width', '0');
+	$('body').css('marginLeft', '0');
+	$('.overlay').css('display', 'none');
 
 }
